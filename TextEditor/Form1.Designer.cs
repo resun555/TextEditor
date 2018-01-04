7249a6cd8342f84ca5cf480ead961640051e8dda
Sam Iris <resun555@live.cn>
2018-01-04 10:38:22 +0800

    Add project files.

---
+++ TextEditor/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace TextEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.openButton = new System.Windows.Forms.Button();
+            this.closeSaveButton = new System.Windows.Forms.Button();
+            this.closeDiscardButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(644, 381);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 2);
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(638, 336);
+            this.textBox1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 345);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(81, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Word Wrap";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.openButton);
+            this.flowLayoutPanel1.Controls.Add(this.closeSaveButton);
+            this.flowLayoutPanel1.Controls.Add(this.closeDiscardButton);
+            this.flowLayoutPanel1.Controls.Add(this.exitButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(99, 345);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(542, 33);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // openButton
+            // 
+            this.openButton.AutoSize = true;
+            this.openButton.Location = new System.Drawing.Point(464, 3);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open...";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // closeSaveButton
+            // 
+            this.closeSaveButton.AutoSize = true;
+            this.closeSaveButton.Location = new System.Drawing.Point(337, 3);
+            this.closeSaveButton.Name = "closeSaveButton";
+            this.closeSaveButton.Size = new System.Drawing.Size(121, 23);
+            this.closeSaveButton.TabIndex = 1;
+            this.closeSaveButton.Text = "Close saving changes";
+            this.closeSaveButton.UseVisualStyleBackColor = true;
+            this.closeSaveButton.Click += new System.EventHandler(this.closeSaveButton_Click);
+            // 
+            // closeDiscardButton
+            // 
+            this.closeDiscardButton.AutoSize = true;
+            this.closeDiscardButton.Location = new System.Drawing.Point(193, 3);
+            this.closeDiscardButton.Name = "closeDiscardButton";
+            this.closeDiscardButton.Size = new System.Drawing.Size(138, 23);
+            this.closeDiscardButton.TabIndex = 2;
+            this.closeDiscardButton.Text = "Close discarding changes";
+            this.closeDiscardButton.UseVisualStyleBackColor = true;
+            this.closeDiscardButton.Click += new System.EventHandler(this.closeDiscardButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.AutoSize = true;
+            this.exitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitButton.Location = new System.Drawing.Point(112, 3);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Text Documents (*.txt)|*.txt|All files (*.*)|*.*";
+            this.openFileDialog1.Title = "Open";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "*.txt";
+            this.saveFileDialog1.Filter = "Text Documents (*.txt)|*.txt|All files (*.*)|*.*";
+            this.saveFileDialog1.Title = "Save As";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 381);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Text Editor";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button closeSaveButton;
+        private System.Windows.Forms.Button closeDiscardButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ TextEditor/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            // Show the Open File dialog. If the user clicks OK, read all text in the
+            // file that the user chose. Retry if the file name is invalid.
+            while (true)
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        textBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Exception: " + ex.Message);
+                        continue;
+                    }
+                }
+            }
+        }
+
+        private void closeSaveButton_Click(object sender, EventArgs e)
+        {
+            // If the user has chosen a file, 
+            // change the Save File Dialog's
+            // FileName property to the Open File Dialog's
+            // FileName property.
+            if (openFileDialog1.FileName != "")
+            {
+                saveFileDialog1.FileName = openFileDialog1.FileName;
+            }
+            // If the user has not chosen a file, 
+            // show the Save File dialog. If the user clicks 
+            // OK or the user has chosen a file, writeout all text to the
+            // file that the user chose. Retry if the file name is invalid.
+            while (true)
+            {
+                if (openFileDialog1.FileName != "" || saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        File.WriteAllText(saveFileDialog1.FileName, textBox1.Text);
+
+                        // Clear the text.
+                        textBox1.Clear();
+
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Exception: " + ex.Message);
+                        continue;
+                    }
+                }
+            }
+        }
+
+        private void closeDiscardButton_Click(object sender, EventArgs e)
+        {
+            // Clear the text.
+            textBox1.Clear();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // If the user has chosen a file, 
+            // compare the content of the file with
+            // the content of the TextBox. If the contents are
+            // different, show a MessageBox to ask the user
+            // whether to save changes or to select cancel
+            if (openFileDialog1.FileName != "")
+            {
+                if (textBox1.Text != File.ReadAllText(openFileDialog1.FileName))
+                {
+                    DialogResult result = MessageBox.Show("Do you want to save changes to " + openFileDialog1.FileName + "?", 
+                                                                                                                   this.Text, 
+                                                                                               MessageBoxButtons.YesNoCancel, 
+                                                                                                     MessageBoxIcon.Question);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        this.closeSaveButton_Click(sender, e);
+                    }
+                    else if (result == DialogResult.No)
+                    {
+                        this.closeDiscardButton_Click(sender, e);
+                    }
+
+                    if (result != DialogResult.Cancel)
+                    {
+                        // Close the form.
+                        this.Close();
+                    }
+                }
+                else
+                {
+                    // Close the form.
+                    this.Close();
+                }
+            }
+            // If the user has not chosen a file, 
+            // check if the content of the TextBox is 
+            // empty. If the content is not
+            // empty, show a MessageBox to ask the user
+            // whether to save changes or to select cancel
+            else
+            {
+                if (textBox1.Text != "")
+                {
+                    const string untitled = "Untitled";
+                    DialogResult result = MessageBox.Show("Do you want to save changes to " + untitled + "?", 
+                                                                                                   this.Text, 
+                                                                               MessageBoxButtons.YesNoCancel, 
+                                                                                     MessageBoxIcon.Question);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        this.closeSaveButton_Click(sender, e);
+                    }
+                    else if (result == DialogResult.No)
+                    {
+                        this.closeDiscardButton_Click(sender, e);
+                    }
+
+                    if (result != DialogResult.Cancel)
+                    {
+                        // Close the form.
+                        this.Close();
+                    }
+                }
+                else
+                {
+                    // Close the form.
+                    this.Close();
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // If the user selects the Word Wrap check box, 
+            // change the TextBox's
+            // WordWrap property to true. If the user clears
+            // the check box, change it to false.
+            if (checkBox1.Checked)
+            {
+                textBox1.WordWrap = true;
+            }
+            else
+            {
+                textBox1.WordWrap = false;
+            }
+        }
+    }
+}
